dd4a409b9e70d58ce59bb2f0723882041a0288ee
Hisun100 <zhu_shaohua100@163.com>
2020-04-15 11:14:20 +0800

    Update api_df_stack_bar.py

--- 数据可视化/柱状图/api_df_stack_bar.py
+++ 数据可视化/柱状图/api_df_stack_bar.py
@@ -13,6 +13,6 @@ month_sum_coupon_consume = dfoff[dfoff['consumeType']==1].groupby('month_consume
 #将month_sum_ordinary_consume和month_sum_coupon_consume,按month_consume字段进行左连接
 month_sum_consume = pd.merge(month_sum_coupon_consume, month_sum_ordinary_consume,on='month_consume')
 #绘制柱状堆叠图--每月优惠券消费次数占总消费次数的比重
-ax22=month_sum_consume.plot(kind='bar',stacked=True)
+ax22=month_sum_consume.plot(kind='bar',x='month_consume',stacked=True,rot=60)
 ax22.set_title('2016年上半年每月优惠券消费次数占比变化')
 ax22.legend(['优惠券消费次数','总消费次数'])
